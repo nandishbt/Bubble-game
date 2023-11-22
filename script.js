37a9c1463fb871c177d8d0a7a0b7bfc140d6ff89
nandishbt <nandishbt2001@gmail.com>
2023-11-22 13:35:58 +0530

    game finished

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+var timerr = 60;
+var score = 0;
+var rn = 0;
+function bubblemake(){
 var clutter = "";
 for(var i = 1;i<=120;i++){
     var num = Math.floor(Math.random() * 10);
@@ -7,3 +11,41 @@ for(var i = 1;i<=120;i++){
 }
 var bub = document.querySelector("#pbtm");
 bub.innerHTML = clutter;
+}
+
+function timerint(){
+    
+    var t = setInterval(function(){
+        if(timerr>0){
+            timerr--;
+            document.querySelector("#Timer").textContent = timerr;
+        }
+        else{
+            clearInterval(t);
+            document.querySelector("#pbtm").innerHTML = `<h1>GAME OVER</h1>`
+        }
+    },1000)
+}
+function hit(){
+    rn = Math.floor(Math.random() * 10);
+    document.querySelector("#hitval").textContent = rn;
+
+    
+
+}
+function scoreincrease(){
+    score+=10;
+    document.querySelector("#score").textContent=score;
+
+}
+bubblemake();
+timerint();
+hit();
+document.querySelector("#pbtm").addEventListener("click",function(dt){
+    var clickednumber = Number(dt.target.textContent);
+    if(clickednumber===rn){
+        scoreincrease();
+        hit();
+        bubblemake();
+    }
+})
